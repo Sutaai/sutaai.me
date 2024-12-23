a25eca612ab3a8b256f35ee386768a40587201ea
Sutaai <sutaai@proton.me>
2024-12-23 13:13:38 +0100

    fix: incorrect site url + missing redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,9 +21,12 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://sutaai.me/",
   base: "/",
   trailingSlash: "always",
+  redirects: {
+    "/397/": "https://sutaai.dev/397/",
+  },
   integrations: [
     tailwind(
         {
