62cb7b81129d4d73e52290ff97328bc163d51a5a
Sutaai <sutaai@proton.me>
2025-05-22 14:26:18 +0200

    Merge remote-tracking branch 'fuwari' into task/update-fuwari

--- src/i18n/languages/fr.ts
+++ src/i18n/languages/fr.ts
@@ -2,37 +2,37 @@ import Key from "../i18nKey";
 import type { Translation } from "../translation";
 
 export const fr: Translation = {
-  [Key.home]: "🏠",
-  [Key.about]: "à propos",
-  [Key.archive]: "archives",
-  [Key.search]: "chercher",
+	[Key.home]: "🏠",
+	[Key.about]: "à propos",
+	[Key.archive]: "archives",
+	[Key.search]: "chercher",
 
-  [Key.tags]: "tags",
-  [Key.categories]: "catégories",
-  [Key.recentPosts]: "posts récents",
+	[Key.tags]: "tags",
+	[Key.categories]: "catégories",
+	[Key.recentPosts]: "posts récents",
 
-  [Key.comments]: "commentaires",
+	[Key.comments]: "commentaires",
 
-  [Key.untitled]: "sans titre",
-  [Key.uncategorized]: "non catégorisé",
-  [Key.noTags]: "sans tags",
+	[Key.untitled]: "sans titre",
+	[Key.uncategorized]: "non catégorisé",
+	[Key.noTags]: "sans tags",
 
-  [Key.wordCount]: "mot",
-  [Key.wordsCount]: "mots",
-  [Key.minuteCount]: "minute",
-  [Key.minutesCount]: "minutes",
-  [Key.postCount]: "post",
-  [Key.postsCount]: "posts",
+	[Key.wordCount]: "mot",
+	[Key.wordsCount]: "mots",
+	[Key.minuteCount]: "minute",
+	[Key.minutesCount]: "minutes",
+	[Key.postCount]: "post",
+	[Key.postsCount]: "posts",
 
-  [Key.themeColor]: "couleur du thème",
+	[Key.themeColor]: "couleur du thème",
 
-  [Key.lightMode]: "clair",
-  [Key.darkMode]: "sombre",
-  [Key.systemMode]: "système",
+	[Key.lightMode]: "clair",
+	[Key.darkMode]: "sombre",
+	[Key.systemMode]: "système",
 
-  [Key.more]: "plus",
+	[Key.more]: "plus",
 
-  [Key.author]: "auteur",
-  [Key.publishedAt]: "publié à",
-  [Key.license]: "license",
+	[Key.author]: "auteur",
+	[Key.publishedAt]: "publié à",
+	[Key.license]: "license",
 };
